41e457f60aed65fc8e96c27f6e79084c9f3cc783
saraenge <sara.e.engelhardt@gmail.com>
2023-06-03 22:13:29 +0200

    Basic spring boot project setup

---
+++ src/main/java/com/saraenge/longestlines/LongestLinesApplication.java
@@ -0,0 +1,13 @@
+package com.saraenge.longestlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongestLinesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LongestLinesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saraenge/longestlines/LongestLinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saraenge.longestlines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongestLinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
