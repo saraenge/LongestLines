7ecc5995ce5a16bcd81377a8628ecbfb039c55f8
saraenge <sara.e.engelhardt@gmail.com>
2023-06-05 22:49:40 +0200

    Example project Spring boot call REST API

--- src/main/java/com/saraenge/longestlines/LongestLinesApplication.java
+++ src/main/java/com/saraenge/longestlines/LongestLinesApplication.java
@@ -1,8 +1,16 @@
 package com.saraenge.longestlines;
 
+import com.saraenge.longestlines.Repository.TodoRepository;
+import com.saraenge.longestlines.model.Todo;
+import com.saraenge.longestlines.service.JSONPlaceholderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 @EnableConfigurationProperties(TrafiklabConfigProperties.class)
@@ -10,6 +18,20 @@ public class LongestLinesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LongestLinesApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JSONPlaceholderService jsonPlaceholderService, TodoRepository todoRepository) {
+        return args -> {
+            List<Todo> todos = jsonPlaceholderService.getTodos();
+
+            todoRepository.saveAll(todos);
+        };
     }
 
 }
