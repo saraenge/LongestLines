1fe40a4d0f03db316da743e0598b5b2cf25bccc7
saraenge <sara.e.engelhardt@gmail.com>
2023-06-08 00:55:13 +0200

    Add deserializer

--- src/main/java/com/saraenge/longestlines/model/JourneyPatternPointOnLine.java
+++ src/main/java/com/saraenge/longestlines/model/JourneyPatternPointOnLine.java
@@ -1,5 +1,9 @@
 package com.saraenge.longestlines.model;
 
-public record JourneyPatternPointOnLine(int lineNumber, int directionCode, int JourneyPatternPointNumber) {
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.saraenge.longestlines.deserializer.JourneyDeserializer;
+
+@JsonDeserialize(using = JourneyDeserializer.class)
+public record JourneyPatternPointOnLine(int lineNumber, int directionCode, int stopPointNumber) {
     // ignorerar "LastModifiedUtcDateTime", "ExistsFromDate"
 }
