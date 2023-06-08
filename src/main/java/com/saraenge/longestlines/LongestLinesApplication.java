32ae9b190ccf39797ba3c05a5d1daae775fa36b9
saraenge <sara.e.engelhardt@gmail.com>
2023-06-08 21:18:45 +0200

    Code cleanup

--- src/main/java/com/saraenge/longestlines/LongestLinesApplication.java
+++ src/main/java/com/saraenge/longestlines/LongestLinesApplication.java
@@ -3,10 +3,7 @@ package com.saraenge.longestlines;
 import com.saraenge.longestlines.model.JourneyPatternPointOnLine;
 import com.saraenge.longestlines.repository.JourneyPatternPointOnLineRepository;
 import com.saraenge.longestlines.repository.StopPointRepository;
-import com.saraenge.longestlines.repository.TodoRepository;
 import com.saraenge.longestlines.model.StopPoint;
-import com.saraenge.longestlines.model.Todo;
-import com.saraenge.longestlines.service.JSONPlaceholderService;
 import com.saraenge.longestlines.service.TrafiklabService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -31,16 +28,7 @@ public class LongestLinesApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(JSONPlaceholderService jsonPlaceholderService, TodoRepository todoRepository) {
-        return args -> {
-            List<Todo> todos = jsonPlaceholderService.getTodos();
-
-            todoRepository.saveAll(todos);
-        };
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunnerStopPoint(TrafiklabService trafiklabService, StopPointRepository stopPointRepository) {
+    CommandLineRunner commandLineRunnerStopPointAPI(TrafiklabService trafiklabService, StopPointRepository stopPointRepository) {
         return args -> {
             List<StopPoint> stopPoints = trafiklabService.getStopPoints();
 
