e71d9458c83d96c85022bc74d3ca6d138037c3be
saraenge <sara.e.engelhardt@gmail.com>
2023-06-14 23:59:49 +0200

    Add a component for info about each line

---
+++ frontend/src/LineRow.js
@@ -0,0 +1,40 @@
+import {useState} from "react";
+import Stops from "./Stops";
+
+const LineRow = ({line, direction, numberOfStops}) => {
+    const [stops, setStops] = useState([]);
+    const [showStops, setShowStops] = useState(false);
+
+    const BASE_URL = 'http://localhost:8080/api/longestLines'
+    const stopsUrl = (line, direction) => `${BASE_URL}/stops?line=${line}&direction=${direction}`;
+
+    const toggleShowStops = () => {
+        setShowStops(!showStops);
+        if (stops.length == 0) {
+            fetchStops();
+        }
+    }
+
+    const fetchStops = () => {
+        return fetch(stopsUrl(line, direction))
+            .then((res) => res.json())
+            .then((l) => setStops(l))
+    }
+
+    return (
+        <div>
+            <button
+                onClick={toggleShowStops}
+            >
+                <p >Linje: {line} Riktning: {direction} Antal hållplatser: {numberOfStops}</p>
+            </button>
+            {showStops && stops &&
+                <Stops
+                    stops={stops.stops}
+                />
+            }
+        </div>
+    );
+};
+
+export default LineRow;
