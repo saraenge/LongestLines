eb420531bcc643b9b669e0ae89f3e033e53b4b34
saraenge <sara.e.engelhardt@gmail.com>
2023-06-15 00:01:54 +0200

    Change the base component to use the new lines component and fetch the data for it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import LineRow from "./LineRow";
 
 function App() {
+  const [lines, setLines] = useState([])
+
+  const BASE_URL = 'http://localhost:8080/api/longestLines'
+  const linesUrl = BASE_URL + '/lines';
+
+  const fetchLines = () => {
+    return fetch(linesUrl)
+        .then((res) => res.json())
+        .then((l) => setLines(l))
+        .catch((error) => console.log('Error fetching lines:', error));
+  }
+
+  useEffect(() => {
+    fetchLines();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Longest lines</h1>
+          {lines?.map((line, arrayIndex) => (
+              <LineRow
+                  key={arrayIndex}
+                  line={line.lineNumber}
+                  direction={line.direction}
+                  numberOfStops={line.numberOfStops}
+              />
+              ))}
+        </header>
+      </div>
   );
 }
 
